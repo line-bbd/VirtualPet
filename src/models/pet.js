4b34ba643bd200382448c5483f62de06c952829a
Line Dorfling <line@bbd.co.za>
2023-05-12 10:20:03 +0200

    api endpoints added for taking care of your pet

--- src/models/pet.js
+++ src/models/pet.js
@@ -1,27 +1,68 @@
 class Pet {
   constructor(name) {
     this.name = name;
-    this.hunger = 100;
-    this.happiness = 100;
     this.health = 100;
-    this.money = 100;
+    this.happiness = 100;
+    this.fed = 100;
+    this.hydrated = 100;
+    this.hygiene = 100;
+    this.energy = 100;
     this.message = "You have a new pet!";
   }
 
   feed() {
-    this.hunger += 10;
-    this.happiness += 10;
-    this.health += 10;
-    this.money -= 10;
+    this.fed += 5;
+    this.happiness += 5;
+    this.energy += 5;
     this.message = "You fed your pet!";
   }
 
-  play() {
-    this.hunger -= 10;
+  giveWater() {
+    this.hydrated += 5;
+    this.happiness += 5;
+    this.energy += 5;
+    this.message = "You gave your pet water!";
+  }
+
+  giveAttention() {
+    this.happiness += 5;
+    this.energy -= 3;
+    this.message = "You gave your pet attention!";
+  }
+
+  giveMedicine() {
+    this.health += 5;
+    this.happiness -= 3;
+    this.energy -= 3;
+    this.message = "You gave your pet medicine!";
+  }
+
+  giveBath() {
+    this.hygiene += 5;
+    this.happiness += 5;
+    this.energy -= 3;
+    this.message = "You gave your pet a bath!";
+  }
+
+  giveTreat() {
+    this.happiness += 5;
+    this.hunger += 2;
+    this.energy -= 3;
+    this.message = "You gave your pet a treat!";
+  }
+
+  giveToy() {
+    this.happiness += 5;
+    this.energy -= 3;
+    this.message = "You gave your pet a toy!";
+  }
+
+  sleep() {
+    this.energy += 20;
     this.happiness += 10;
-    this.health += 10;
-    this.money -= 10;
-    this.message = "You played with your pet!";
+    this.hunger -= 5;
+    this.hydrated -= 5;
+    this.message = "You put your pet to bed!";
   }
 }
 
